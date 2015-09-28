ddd1590e603ab083a6654ee287a41b6ec62f427d
Max Huang <sakana@cycu.org.tw>
2015-09-28 16:46:17 -0500

    Add mind map

--- Mindmap/ansible20150928.mm
+++ Mindmap/ansible20150928.mm
@@ -40,5 +40,173 @@
 <node CREATED="1443468098836" ID="ID_755094239" MODIFIED="1443468111185" TEXT="#ansible on irc.freenode.net"/>
 </node>
 </node>
+<node CREATED="1443470084667" ID="ID_176320990" MODIFIED="1443470090701" POSITION="left" TEXT="playbook">
+<node CREATED="1443470091156" ID="ID_346826957" MODIFIED="1443470188680">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      A script is called a playbook.
+    </p>
+    <p>
+      A playbook describes which hosts( what Ansible calls remote servers ) to configure, and an ordered list of tasks to perform on those hosts.
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node CREATED="1443470513873" ID="ID_1570061213" MODIFIED="1443470586664">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <ul>
+      <li>
+        Ansible runs each task in parallel across all hosts.
+      </li>
+      <li>
+        Ansible waits until all hosts have completed a task before moving to the next task.
+      </li>
+      <li>
+        Ansible runs the tasks in the order that you specify them.
+      </li>
+    </ul>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+<node CREATED="1443470738672" ID="ID_1152545268" MODIFIED="1443470751089" POSITION="left" TEXT="Why ansible?">
+<node CREATED="1443470751569" ID="ID_1575804634" MODIFIED="1443470759514" TEXT="Easy to read syntax"/>
+<node CREATED="1443470796470" ID="ID_219203845" MODIFIED="1443470808300" TEXT="Nothing to install on the remote hosts">
+<node CREATED="1443470873402" ID="ID_361281942" MODIFIED="1443470914400">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      To manage a server with Ansible
+    </p>
+  </body>
+</html>
+</richcontent>
+<node CREATED="1443470916851" ID="ID_1353096803" MODIFIED="1443470940870" TEXT="Needs to have SSH and Python 2.5 or later installed."/>
+</node>
+<node CREATED="1443470942181" ID="ID_59975773" MODIFIED="1443470950030" TEXT="The control machine">
+<node CREATED="1443470981333" ID="ID_1039562262" MODIFIED="1443470993675" TEXT="The one that you use to control remote machines"/>
+<node CREATED="1443470956165" ID="ID_778493953" MODIFIED="1443470971627" TEXT="needs to have python 2.6 or later installed."/>
+</node>
+</node>
+<node CREATED="1443471010175" ID="ID_1685536592" MODIFIED="1443471154779" TEXT="Push-based">
+<node CREATED="1443471155644" ID="ID_260508288" MODIFIED="1443471284697">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Making change looks like this:
+    </p>
+    <ol>
+      <li>
+        You: Make a change to a playbook.
+      </li>
+      <li>
+        You: run the new playbook.
+      </li>
+      <li>
+        Ansible: connects to servers and executes modules, which changes server state.
+      </li>
+    </ol>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+<node CREATED="1443471320676" ID="ID_1217415885" MODIFIED="1443471327687" TEXT="Ansible Scales Down"/>
+<node CREATED="1443471364068" ID="ID_1936354818" MODIFIED="1443471370027" TEXT="Built-in Modules"/>
+<node CREATED="1443471590980" ID="ID_503143385" MODIFIED="1443471600955" TEXT="Very thin layer of abstraction">
+<node CREATED="1443471892122" ID="ID_1297863576" MODIFIED="1443472146237">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Ansible focus on writing playbooks that designed to run on specific operating system.
+    </p>
+    <p>
+      You don't need to learn a new set of abstractions that hide the differences between operating systems.
+    </p>
+    <p>
+      The primary unit of reuse in the Ansible community is the module.
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+</node>
+<node CREATED="1443472410816" ID="ID_828756853" MODIFIED="1443472424320" POSITION="left" TEXT="What do I need to know?">
+<node CREATED="1443472424916" ID="ID_1715071762" MODIFIED="1443472524671">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <ul>
+      <li>
+        Connect to a remote machine using SSH.
+      </li>
+      <li>
+        Interact with the bash command-line shell (pipes and redirection).
+      </li>
+      <li>
+        Install packages.
+      </li>
+      <li>
+        Use the sudo command.
+      </li>
+      <li>
+        Check and set file permissions.
+      </li>
+      <li>
+        Start and stop services.
+      </li>
+      <li>
+        Set environment variables.
+      </li>
+      <li>
+        Write scripts ( Any language )
+      </li>
+    </ul>
+  </body>
+</html>
+</richcontent>
+</node>
+<node CREATED="1443472579370" ID="ID_1403784683" MODIFIED="1443472645308">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      You don't need to know python to use Ansible unless you want to write your own module.
+    </p>
+    <p>
+      You will need to learn some YAML and Jinja2 to use Ansible.
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+<node CREATED="1443476517040" ID="ID_1361488639" MODIFIED="1443476522850" POSITION="right" TEXT="Install Ansible">
+<node CREATED="1443476523442" ID="ID_1841966144" MODIFIED="1443476531925" TEXT="http://docs.ansible.com/ansible/intro_installation.html"/>
+</node>
 </node>
 </map>
