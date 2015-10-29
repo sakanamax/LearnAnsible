8e738ccd5c6430c2e944c872626752f288da2098
Max Huang <sakana@cycu.org.tw>
2015-10-29 17:02:49 -0500

    Add mind map and practice file

---
+++ books/Oreilly-Up-And-Running-Ansible/playbooks/scripts/ls.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+ls /boot >> ~/ls.txt
+
+echo "-----------" >> ~/ls.txt
+echo "\$0 is $0"   >> ~/ls.txt
+echo "\$1 is $1"   >> ~/ls.txt
+echo "\$2 is $2"   >> ~/ls.txt
+echo "-----------" >> ~/ls.txt
+
